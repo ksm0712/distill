eae1a11de8b3ca253732e44b9c501a746adaf9c2
ksm0712 <karansm0712@gmail.com>
2026-04-30 20:36:55 -0400

    Initial commit

---
+++ popup.js
@@ -0,0 +1,10 @@
+const explainBtn = document.getElementById("explain-btn")
+const newTopicBtn = document.getElementById("new-topic-btn")
+const customPrompt = document.getElementById("custom-prompt")
+
+explainBtn.addEventListener('click', function() {
+    console.log("Explain clicked")
+})
+newTopicBtn.addEventListener('click', function() {
+    console.log("New Topic clicked")
+})
